bd73d9d505e20515f30541285426e5e4295b3d41
dylanwe <41340521+dylanwe@users.noreply.github.com>
2019-12-15 18:35:04 +0100

    !joke command

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const jokes = require('./jokes');
+
 
 bot.login('NjU1NTQzNjg4NjU4OTQ0MDEx.XfVzYQ.C6e-PSLqmMd7jDNSRO0IePBe828');
 
@@ -16,4 +18,12 @@ bot.on('message', msg => {
     dad("im")
     dad("I'm")
     dad("Im")
+    dad("I’m")
+    dad("i’m")
+    dad("I am")
+    
+    if (userMsg === "!joke") {
+        let random = Math.floor(Math.random() * jokes.length);
+        msg.reply(jokes[random]);
+    }
 });
